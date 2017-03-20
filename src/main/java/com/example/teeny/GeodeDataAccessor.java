7b9c75749c604ca06f347c3536506a0d99e9f1b3
Swapnil Bawaskar <sbawaskar@pivotal.io>
2017-03-20 14:02:02 -0700

    stubbing out the implementation.

--- src/main/java/com/example/teeny/GeodeDataAccessor.java
+++ src/main/java/com/example/teeny/GeodeDataAccessor.java
@@ -14,59 +14,35 @@ import org.springframework.stereotype.Component;
 @Component
 @Primary
 public class GeodeDataAccessor implements DataAccessor {
-  String LOCATOR = System.getProperty("locator");
-  String SERVER = System.getProperty("server");
-  int PORT = Integer.getInteger("port", 1527);
-  String REGION_TYPE = System.getProperty("region_type", ClientRegionShortcut.LOCAL.name());
 
   ClientCache cache;
   Region<String, String> teenyRegion;
   Region<String, Integer> statsRegion;
 
   public GeodeDataAccessor() {
-    if (LOCATOR != null) {
-      cache = new ClientCacheFactory().addPoolLocator(LOCATOR, PORT).create();
-    } else if (SERVER != null) {
-      cache = new ClientCacheFactory().addPoolServer(LOCATOR, PORT).create();
-    } else {
-      cache = new ClientCacheFactory().create();
-    }
+    cache = new ClientCacheFactory().addPoolLocator("localhost", 10334).create();
 
-    ClientRegionFactory<String, String> teenyFactory = cache.createClientRegionFactory(REGION_TYPE);
-    teenyRegion = teenyFactory.create("teeny");
-
-    ClientRegionFactory<String, Integer> statsFactory = cache.createClientRegionFactory(REGION_TYPE);
-    statsRegion = statsFactory.create("stats");
   }
 
   @Override
   public void addTeeny(Teeny teeny) {
-    teenyRegion.put(teeny.getId(), teeny.getUrl());
-    statsRegion.put(teeny.getId(), 0);
+
   }
 
   @Override
   public Teeny accessTeeny(String id) {
-    String url = teenyRegion.get(id);
-    int count = statsRegion.get(id);
-    statsRegion.put(id, ++count);
-    return new Teeny(url, count);
+    return null;
   }
 
   @Override
   public int size() {
-    return teenyRegion.size();
+    return teenyRegion.keySetOnServer().size();
   }
 
   @Override
   public Teeny[] getAll() {
     Teeny[] result = new Teeny[size()];
-    int i = 0;
-    for (Entry<String, String> entry : teenyRegion.entrySet()) {
-      String url = entry.getValue();
-      int count = statsRegion.get(entry.getKey());
-      result[i++] = new Teeny(url, count);
-    }
+
     return result;
   }
 
@@ -78,7 +54,6 @@ public class GeodeDataAccessor implements DataAccessor {
 
   @Override
   public Teeny removeTeeny(String id) {
-    String url = teenyRegion.remove(id);
-    return new Teeny(url);
+    return null;
   }
 }
