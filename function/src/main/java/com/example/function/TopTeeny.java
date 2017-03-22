cc65dfa5857ed5b0cf8eb31b6e22c51bb7054c2f
Swapnil Bawaskar <sbawaskar@pivotal.io>
2017-03-22 16:17:26 -0700

    adding function sub project

---
+++ function/src/main/java/com/example/function/TopTeeny.java
@@ -0,0 +1,14 @@
+package com.example.function;
+
+import org.apache.geode.cache.execute.Function;
+import org.apache.geode.cache.execute.FunctionContext;
+
+/**
+ * Created by sbawaskar on 3/22/17.
+ */
+public class TopTeeny implements Function {
+  @Override
+  public void execute(FunctionContext context) {
+
+  }
+}
